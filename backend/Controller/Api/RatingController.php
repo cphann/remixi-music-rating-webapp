ed4b3b1e90c0162b7ae828c7a5a9d23822567f9c
nagena <nlatifi@wesleyan.edu>
2023-10-27 14:07:16 -0400

    adding backend files in backend folder

---
+++ backend/Controller/Api/RatingController.php
@@ -0,0 +1,41 @@
+<?php
+class RatingController extends BaseController
+{
+    /** 
+* "/ratings/list" Endpoint - Get list of ratings 
+*/
+    public function listAction()
+    {
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        $arrQueryStringParams = $this->getQueryStringParams();
+        if (strtoupper($requestMethod) == 'GET') {
+            try {
+                $ratingModel = new RatingModel();
+                $intLimit = 10;
+                if (isset($arrQueryStringParams['limit']) && $arrQueryStringParams['limit']) {
+                    $intLimit = $arrQueryStringParams['limit'];
+                }
+                $arrRatings = $ratingModel->getRatings($intLimit);
+                $responseData = json_encode($arrRatings);
+            } catch (Error $e) {
+                $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+                $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+            }
+        } else {
+            $strErrorDesc = 'Method not supported';
+            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+        // send output 
+        if (!$strErrorDesc) {
+            $this->sendOutput(
+                $responseData,
+                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+            );
+        } else {
+            $this->sendOutput(json_encode(array('error' => $strErrorDesc)), 
+                array('Content-Type: application/json', $strErrorHeader)
+            );
+        }
+    }
+}
