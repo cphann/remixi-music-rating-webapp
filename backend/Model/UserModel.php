90bce736eb393d745f7d045d4f4b17bc2b29cd01
Chi Phan <cphan@wesleyan.edu>
2023-10-30 22:00:50 -0400

    added registerAction

--- backend/Model/UserModel.php
+++ backend/Model/UserModel.php
@@ -6,25 +6,47 @@ class UserModel extends Database
     {
         return $this->select("SELECT * FROM users ORDER BY username ASC LIMIT ?", ["i", $limit]);
     }
-    public function createUsers($limit)
-    {
-        return $this->insert("SELECT * FROM users ORDER BY username ASC LIMIT ?", ["i", $limit]);
-    }
+    
+    // public function createUsers($limit)
+    // {
+    //     return $this->insert("SELECT * FROM users ORDER BY username ASC LIMIT ?", ["i", $limit]);
+    // }
 
     public function isUserRegistered($username) {
+        // $query = "SELECT * FROM users WHERE username = ?";
+        // $stmt = mysqli_prepare($conn, $query);
+        // mysqli_stmt_bind_param($stmt, "s", $username);
+        // mysqli_stmt_execute($stmt);
+        // $result = mysqli_stmt_get_result($stmt);
+        // return (mysqli_num_rows($result) > 0);
+
         $query = "SELECT * FROM users WHERE username = ?";
-        $stmt = mysqli_prepare($conn, $query);
-        mysqli_stmt_bind_param($stmt, "s", $username);
-        mysqli_stmt_execute($stmt);
-        $result = mysqli_stmt_get_result($stmt);
-        return (mysqli_num_rows($result) > 0);
+        $params = ['s', $username]; // 's' indicates string data type
+
+        $result = $this->select($query, $params);
+
+        return count($result) > 0;
     }
 
-    public function createUser($userData)
+    public function registerUser($userData)
     {
+        // $insert_query = "INSERT INTO users (username, pass, salt) VALUES (?, ?, ?)";
+        // $insert_stmt = mysqli_prepare($conn, $insert_query);
+        // mysqli_stmt_bind_param($insert_stmt, "sss", $username, $hashed_password, $salt);
+        // mysqli_stmt_execute($insert_stmt);
+
         $insert_query = "INSERT INTO users (username, pass, salt) VALUES (?, ?, ?)";
-        $insert_stmt = mysqli_prepare($conn, $insert_query);
-        mysqli_stmt_bind_param($insert_stmt, "sss", $username, $hashed_password, $salt);
-        mysqli_stmt_execute($insert_stmt);
+        $params = ['sss', $username, $hashed_password, $salt];
+
+        // Execute the database insert operation
+        try {
+            $this->executeStatement($insert_query, $params);
+            // Return true to indicate successful registration
+            return true;
+        } catch (Exception $e) {
+            // Handle any database errors and return false to indicate registration failure
+            return false;
+        }    
     }
+
 }
