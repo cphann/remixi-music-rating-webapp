eaf50c8989f91212612a21847ed9016542271ba3
Chi Phan <cphan@wesleyan.edu>
2023-11-01 14:12:01 -0400

    connected view rating frontend and backend

---
+++ remixi/src/ViewRating.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
+function ViewRating() {
+  const [rating, setRating] = useState(null);
+  const { id } = useParams(); // Get the rating ID from the URL
+
+  useEffect(() => {
+    const fetchRating = async () => {
+        try {
+            console.log("Fetching rating for ID:", id); // debug log
+            const response = await axios.get(`http://localhost:8080/comp333_hw3/backend/index.php/ratings/view?id=${id}`);
+            console.log("Response data:", response.data); // debug log
+            if (response.data && response.data.id) {
+                setRating(response.data);
+            } else {
+                console.error("Received unexpected data from backend:", response.data);
+            }
+        } catch (error) {
+            console.error("Error fetching rating:", error);
+        }
+    };
+
+    fetchRating();
+}, [id]);
+
+  return (
+    <div>
+      <h2>Rating Details</h2>
+      {rating && (
+        <>
+          <p><strong>ID:</strong> {rating.id}</p>
+          <p><strong>Artist:</strong> {rating.artist}</p>
+          <p><strong>Song:</strong> {rating.song}</p>
+          <p><strong>Rating:</strong> {rating.rating}</p>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default ViewRating;
