5887ec4bfcabed8da58108d77a2b706b58da8b9f
nagena <nlatifi@wesleyan.edu>
2023-11-01 00:39:56 -0400

    updating homepage.js and app.js

--- remixi/src/App.js
+++ remixi/src/App.js
@@ -26,21 +26,12 @@ export default App;*/
 
 import React from 'react';
 import HomePage from './homepage';
-import Signup from './signup';
-import Login from './Login';
-import DisplayRating from './DisplayRating';
-import AddRating from './AddRating';
 
 function App() {
   return (
     <div className="App">
-      <h1>Ratings App</h1>
-      <HomePage />
-      <Login />
-      <Logout />
-      <DisplayRatings /> {/* Example, you can fetch ratingId dynamically */}
-      <AddRating />
-      
+      <h1>Remixi</h1>
+      <HomePage />      
     </div>
   );
 }
--- remixi/src/homepage.js
+++ remixi/src/homepage.js
@@ -1,41 +1,60 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import DeleteRating from './DeleteRating';
-import UpdateRating from './UpdateRating';
-
-function HomePage() {
-  const [ratings, setRatings] = useState([]);
-  const loggedInUser = localStorage.getItem('username');  // Assuming the logged-in username is stored in local storage
-
-  useEffect(() => {
-    const fetchRatings = async () => {
-      try {
-        const response = await axios.get('http://your-backend-url/ratings');
-        setRatings(response.data);
-      } catch (error) {
-        // Handle error
-      }
-    };
-
-    fetchRatings();
-  }, []);
-
-  return (
-    <div>
-      <h2>Your Ratings</h2>
-      {ratings.map(rating => (
-        <div key={rating.id}>
-          <p>Artist: {rating.artist}</p>
-          <p>Song: {rating.song}</p>
-          <p>Rating: {rating.rating}</p>
-          {rating.username === loggedInUser && (
+//import DeleteRating from './DeleteRating';
+//import UpdateRating from './UpdateRating';
+/* add after each rating if user logged in made rating
+            {rating.username === loggedInUser && (
             <>
               <DeleteRating ratingId={rating.id} />
               <UpdateRating ratingId={rating.id} />
             </>
-          )}
-        </div>
-      ))}
+          )}*/
+
+function HomePage() {
+  const [ratings, setRatings] = useState([]);
+  //const loggedInUser = localStorage.getItem('username');  // Assuming the logged-in username is stored in local storage
+
+  useEffect(() => {
+    const fetchRatings = async () => {
+        try {
+            const response = await axios.get('http://localhost/comp333_hw3/backend/index.php/ratings/list?limit=100');
+            console.log("Backend response:", response.data);
+            if (Array.isArray(response.data)) {
+                setRatings(response.data);
+            } else {
+                console.error("Received non-array data from backend:", response.data);
+            }
+        } catch (error) {
+            console.error("Error fetching ratings:", error);
+        }
+    };
+
+    fetchRatings();
+}, []);
+
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Artist</th>
+          <th>Song</th>
+          <th>Rating</th>
+        </tr>
+      </thead>
+      <tbody>
+        {Array.isArray(ratings) && ratings.map((rating) => (
+          <tr key={rating.id}>
+            <td>{rating.id}</td>
+            <td>{rating.artist}</td>
+            <td>{rating.song}</td>
+            <td>{rating.rating}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
     </div>
   );
 }
